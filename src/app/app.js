9e2c63c1e6906777b3b8fbe39b4cd0d8171c3296
youngmelis <66873749+youngmelis@users.noreply.github.com>
2022-08-24 11:43:53 +0200

    add rate saving and style changing

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,11 @@
 const feedback = document.querySelector('.flex-feedback')
 const thankyou = document.querySelector('.flex-thankyou')
 const rated = document.querySelector('#rate')
+const rateOne = document.querySelector('#rate-one')
+const rateTwo = document.querySelector('#rate-two')
+const rateThree = document.querySelector('#rate-three')
+const rateFour = document.querySelector('#rate-four')
+const rateFive = document.querySelector('#rate-five')
 
 
 function thankyouPage() {
@@ -8,6 +13,31 @@ function thankyouPage() {
     thankyou.classList.remove('display-none')
 }
 
-const rating = 3
+function rateSaver(vote){
+    rated.innerText = vote
+}
 
-rated.innerText = rating
+function circleClickStyle(rate){
+    rate.style.backgroundColor = "hsl(217, 12%, 63%)"
+    rate.style.color = "hsl(0, 0%, 100%)"
+}
+
+rateOne.addEventListener('click', () => {
+    circleClickStyle(rateOne)
+})
+
+rateTwo.addEventListener('click', () => {
+    circleClickStyle(rateTwo)
+})
+
+rateThree.addEventListener('click', () => {
+    circleClickStyle(rateThree)
+})
+
+rateFour.addEventListener('click', () => {
+    circleClickStyle(rateFour)
+})
+
+rateFive.addEventListener('click', () => {
+    circleClickStyle(rateFive)
+})
